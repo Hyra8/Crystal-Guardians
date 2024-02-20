cb0975830a30729bea165e5e17329efd968482ed
Hyra8 <146791989+Hyra8@users.noreply.github.com>
2024-02-20 22:23:44 +0700

    delete

--- Assets/Scripts/Attacks/Attack.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Attack : MonoBehaviour
-{
-    [SerializeField] public string targetTag;
-    [SerializeField] public int damage = 5;
-}
